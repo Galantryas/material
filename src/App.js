1a4c0c9a747a56b707fe3073af548b05ba7a1735
Galantryas <galantryas.2014@gmail.com>
2019-05-06 08:53:22 +0700

    card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React, {Component} from 'react';
 // import './App.css';
 import Navbar from'./MyAppBar.js';
+import Card from'./FoodCard.js';
 class App extends Component {
+    constructor(){
+        super();
+        this.state ={
+            foods : [
+                {
+                    title : 'Fried Chicken di geprek',
+                    date : '32 Desember 2099',
+                    description : 'puasa woy'
+                },
+                {
+                    title : 'Fried Chicken di tongseng with Ketchup',
+                    date : '00 Januari 2999',
+                    description : 'masih puasa woy'
+                }
+            ]
+        }
+    }
+
     render() {
         return(
+            <div>
                 <Navbar/>
+                <br/>
+                {this.state.foods.map((data)=>{
+                    return(
+                        <div>
+                            <Card title={data.title} date={data.date} description={data.description}/>
+                            <br/>
+                        </div>
+                    )
+                })}
+                
+            </div>
+
         );  
     }
 }
